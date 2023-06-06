ab9a869da9214380d4f66cd54cf186fb4ce3f9e8
Yoki Hidayatur Rohman <yokihidayaturr13@gmail.com>
2023-06-06 15:53:34 +0700

    s

--- resources/views/layouts/admin/barang/add.blade.php
+++ resources/views/layouts/admin/barang/add.blade.php
@@ -24,11 +24,11 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Tambah Tanaman</h1>
+                <h1>Tambah Barang</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Tanaman</a></li>
+                    <li class="breadcrumb-item"><a href="#">Barang</a></li>
                     <li class="breadcrumb-item active">Tambah</li>
                 </ol>
             </div>
@@ -43,7 +43,7 @@
             <div class="card-header">
                 <div class="row">
                     <div class="col-12">
-                        <h3 class="card-title">Tambah Tanaman</h3>
+                        <h3 class="card-title">Tambah Barang</h3>
                     </div>
                 </div>
             </div>
--- resources/views/layouts/admin/barang/view.blade.php
+++ resources/views/layouts/admin/barang/view.blade.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Tanaman</h1>
+                <h1>Barang</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Master</a></li>
-                    <li class="breadcrumb-item active">Tanaman</li>
+                    <li class="breadcrumb-item active">Barang</li>
                 </ol>
             </div>
         </div>
@@ -49,7 +49,7 @@
                                     <thead>
                                         <tr class="text-center">
                                             <th></th>
-                                            <th>Nama Tanaman</th>
+                                            <th>Nama Barang</th>
                                             <th>Kode</th>
                                             <th width="200px">Opsi</th>
                                         </tr>
